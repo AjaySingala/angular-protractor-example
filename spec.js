7112125113864033cf95cddc677abaddadee2272
AjaySingala <ajay.singala@gmail.com>
2019-09-07 17:04:17 +0530

    protractor working

--- spec.js
+++ spec.js
@@ -1,21 +1,21 @@
-describe('Protractor Demo App', function () {
+describe('Protractor Demo App', function() {
     var firstNumber = element(by.model('first'));
     var secondNumber = element(by.model('second'));
     var goButton = element(by.id('gobutton'));
     var latestResult = element(by.binding('latest'));
 
-    beforeEach(function () {
+    beforeEach(function() {
         browser.get('http://juliemr.github.io/protractor-demo/');
     });
 
 
-    it('should have a title', function () {
-        //browser.get('http://juliemr.github.io/protractor-demo/');
+    it('should have a title', function() {
+        browser.get('http://juliemr.github.io/protractor-demo/');
         expect(browser.getTitle()).toEqual('Super Calculator');
     });
 
-    it('should add one and two', function () {
-        //browser.get('http://juliemr.github.io/protractor-demo/');
+    it('should add one and two', function() {
+        browser.get('http://juliemr.github.io/protractor-demo/');
         element(by.model('first')).sendKeys(1);
         element(by.model('second')).sendKeys(2);
 
@@ -23,7 +23,7 @@ describe('Protractor Demo App', function () {
 
         expect(element(by.binding('latest')).getText()).
             //toEqual('5'); // This is wrong!
-            toEqual('3'); // This is right!
+        toEqual('3'); // This is right!
     });
 
     it('should add four and six', function() {
@@ -33,10 +33,10 @@ describe('Protractor Demo App', function () {
         element(by.id('gobutton')).click();
 
         expect(latestResult.getText()).toEqual('10');
-      });
-    
-      it('should read the value from an input', function() {
+    });
+
+    it('should read the value from an input', function() {
         firstNumber.sendKeys(1);
         expect(firstNumber.getAttribute('value')).toEqual('1');
-      });
+    });
 });
